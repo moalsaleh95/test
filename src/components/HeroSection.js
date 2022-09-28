659f70696c36bfb97cdc0f3056076af7e95204c7
mo.alsaleh <99362725+moalsaleh95@users.noreply.github.com>
2022-09-29 00:04:41 +0300

    8th commit

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,9 +2,7 @@ import React from 'react'
 import hero from '../assets/hero.png';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import PhoneInputGfg from './PhoneNum';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+// import PhoneInputGfg from './PhoneNum';
 import Container from 'react-bootstrap/Container';
 
 function HeroSection() {
@@ -13,31 +11,29 @@ function HeroSection() {
     // style={{ "position":"relative"}}
     >
 
-      
-
       <div className='d-flex mx-auto justify-content-between align-content-center'>
 
         <div>
           <div className='border d-flex flex-wrap flex-col align-items-center text-left '>
             <div >
-              <h2 className='primary-font-color text-start mt-5'>Being A</h2>
-              <h2 className='primary-font-color text-start'>Father Is</h2>
-              <h2 className='primary-font-color text-start'>Everyone’s Right</h2>
-              <h6 className='secondary-font-color text-start'>Reach for your dreams in experienced hands</h6>
+              <h2 className='primary-font-color text-start mt-5' style={{"font-size":"clamp(14px, 3.5vw, 3rem)"}}>Being A</h2>
+              <h2 className='primary-font-color text-start' style={{"font-size":"clamp(14px, 3.5vw, 5rem)"}}>Father Is</h2>
+              <h2 className='primary-font-color text-start' style={{"font-size":"clamp(14px, 3.5vw, 5rem)"}}>Everyone’s Right</h2>
+              <h6 className='secondary-font-color text-start' style={{"font-size":"clamp(7px, 1.6vw, 5rem)"}}>Reach for your dreams in experienced hands</h6>
             </div>
           </div>
 
           <div className='mt-4 '>
             <div className='border d-flex flex-wrap'>
               <Form.Group className="mb-3 me-2">
-                <Form.Control type="text" placeholder="Name Surname" style={{"display":"inline",'border-radius':'25px', "width":"18vw", "background":"#F2F2F2", "color":"#D1D1D1"}} className='flex-fill py-3 px-3' />
+                <Form.Control type="text" placeholder="Name Surname" style={{"display":"inline",'border-radius':'25px', "width":"48vw","height":"2.6rem", "background":"#F2F2F2", "color":"#D1D1D1"}} className='flex-fill py-3 px-3' />
               </Form.Group>
 
               <Form.Group className="mb-3 me-2">
-                <Form.Control type="tel" placeholder="+90" style={{"display":"inline", 'border-radius':'25px', "width":"18vw", "background":"#F2F2F2", "color":"#D1D1D1"}} className='flex-fill py-3 px-3' />
+                <Form.Control type="tel" placeholder="+90" style={{"display":"inline", 'border-radius':'25px', "width":"48vw","height":"2.6rem", "background":"#F2F2F2", "color":"#D1D1D1"}} className='flex-fill py-3 px-3' />
               </Form.Group>
 
-              <Button type="submit" style={{"display":"inline", 'border-radius':'25px', "width":"18vw", "height":"3.6rem"}} className='py-2 px-3 red-btn-color ' >
+              <Button type="submit" style={{"display":"inline", 'border-radius':'25px', "width":"48vw", "height":"2.6rem"}} className='py-2 px-3 red-btn-color ' >
                 Send Now
               </Button>
             </div>
@@ -46,34 +42,23 @@ function HeroSection() {
         </div>
 
         <div>
-          <div className='float-end'>
+          <div className='float-end border' style={{"overflow":"hidden"}}>
             <img alt='hero' src={hero} 
-            className=''
-            style={{"width": "100%","max-width": "400px", "height": "auto"}}
-            // style={{"position":"absolute", "border-radius":"1445px", "z-index":"5", "left":"3em"}} 
-            width="350vw" max-width='100%' height="auto" />
+            className='img-fluid navbar-image'
+              style={{
+                // "width": "100%",
+                "max-width": "210px", 
+                "height": "auto",
+                "border-radius":"1445px",
+                // "position":"absolute",
+                "z-index":"5",
+                "transform":"translateX(7%)",
+              }}
+            // style={{"position":"absolute", "border-radius":"1445px", "z-index":"5", "left":"3em"}} width="350vw" max-width='100%' height="auto" 
+            />
             </div>
         </div>
       </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     </Container>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,49 +8,17 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function NavScrollExample() {
   return (
-    // <Navbar style={{"background":"white"}} className="border border-danger justify-content-center" expand="lg">
-
-    //   <Container fluid className="d-flex justify-content-between">
-
-    //     <Row className='flex-grow-1 flex-shrink-1 align-items-center '>
-    //       <Col >
-    //         <Navbar.Brand href="#"  >
-    //           <img alt='logo' src={logo} width="300" className='border img-fluid' />
-    //         </Navbar.Brand>
-    //       </Col>
-          
-    //       {/* <Col> 
-            // <Nav.Link href="#action2" style={{"border-radius":"25px", "font-size": "18px", "margin":"1rem 0em", "padding":"6px 1.5em"}} className='d-lg-none  text-white primary-bg-color'>WHATSAPP</Nav.Link>
-    //       </Col> */}
-
-    //       <Col > 
-    //         <Navbar.Collapse id="navbarScroll">
-    //         {/* <Navbar.Toggle aria-controls="navbarScroll" /> */}
-            
-    //       <Nav className="my-2 my-lg-0 " style={{ maxHeight: '100px' }} navbarScroll> 
-    //         <Nav.Link style={{"color":"#005B8B"}} href="#action1">Home</Nav.Link>
-    //         <Nav.Link href="#action2">About</Nav.Link>
-    //         <Nav.Link href="#action2">Services</Nav.Link>
-    //         <Nav.Link href="#action2">Team</Nav.Link>
-    //         <Nav.Link href="#action2" style={{"border-radius":"25px", "font-size": "20px", "margin":"0 1em"}} className='px-4 text-white primary-bg-color'>WHATSAPP</Nav.Link>
-    //       </Nav>
-    //     </Navbar.Collapse>
-    //   </Col> 
-
-    //   </Row>
-
-    //   </Container>
-
-    // </Navbar>
-
       <Navbar bg="white" expand="lg">
-      <Container className='border'>
+      <Container className=''>
       <Navbar.Brand href="#"  >
-        <img alt='logo' src={logo} width="50%" className='border ' style={{"width": "100%",
-          "max-width": "100px", "height": "auto"}} />
+        <img alt='logo' src={logo} 
+        // width="50%" 
+        className='img-fluid navbar-image' 
+        style={{"width": "100%", "max-width": "150px", "height": "auto"}} 
+        />
         </Navbar.Brand>
         {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
-        <Nav.Link href="#action2" style={{"border-radius":"25px", "font-size": "18px", "margin":"1rem 0em", "padding":"6px 1.5em"}} className='d-lg-none  text-white primary-bg-color'>WHATSAPP</Nav.Link>
+        <Nav.Link href="#action2" style={{"border-radius":"25px", "font-size": "13px", "margin":"1rem 0em", "padding":"6px 1.5em"}} className='d-lg-none navbar-whatsapp-button text-white primary-bg-color'>WHATSAPP</Nav.Link>
 
         <Navbar.Collapse id="basic-navbar-nav justify-content-end">
           <Nav className="ms-auto">
