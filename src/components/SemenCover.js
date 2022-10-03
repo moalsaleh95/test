c0d905c6957ebb4c02ec8f4dbd4a67a7be630ba6
unknown <mo.alsaleh95@gmail.com>
2022-10-03 11:49:42 +0300

    10th

--- src/components/SemenCover.js
+++ src/components/SemenCover.js
@@ -5,38 +5,19 @@ import Container from 'react-bootstrap/Container';
 
 function SemenCover() {
   return (
-    // <Container className='d-flex justify-content-center my-5' 
-    // style={{"background-color":"#AED9ED", "padding":"2.5rem 0"}}>
-    //     <Row className=''>
-    //         <Col lg={4} className='' style={{"padding":"0 5rem", "margin-top":"1rem"}}>
-    //             <h1 className='ps-2 circle' >25+</h1>
-    //             <p className='ps-2 text '>Years of On-going Experience</p>
-    //         </Col>
-    //         <Col lg={4} className='' style={{"padding":"0 5rem", "margin-top":"1rem"}}>
-    //             <h1 className='ps-2 circle'>70+</h1>
-    //             <p className='ps-2 text'>Countries Worldwide</p>
-    //         </Col>
-    //         <Col lg={4} className='' style={{"padding":"0 5rem", "margin-top":"1rem"}}>
-    //             <h1 className='ps-2 circle'>1600+</h1>
-    //             <p className='ps-2 text'>MicroTESE Performed</p>
-    //         </Col>
-    //     </Row>
-    // </Container>
-
-
     <Container fluid style={{"background-color":"#AED9ED", "padding":"2.5rem 0"}} className='mx-auto'>
-    <Row className='screen-width mx-auto border gy-3 d-flex flex-row' style={{"align-items":"baseline"}}>
-        <Col  className='border ' >
+    <Row className='screen-width mx-auto  gy-3 d-flex flex-row' style={{"align-items":"baseline"}}>
+        <Col  className=' ' >
             <h1 className='ps-2 circle mx-auto' >25+</h1>
             <p className='ps-2 text  text-center' style={{"transform": "translateX(20%)"}}>Years of On-going Experience</p>
         </Col>
-        <Col  className='border ' >
+        <Col  className=' ' >
             <h1 className='ps-2 circle mx-auto'>70+</h1>
             <p className='ps-2 text text-center' style={{"transform": "translateX(12%)"}}>Countries Worldwide</p>
         </Col>
-        <Col  className='border ' >
-            <h1 className='ps-2 circle mx-auto'>1600+</h1>
-            <p className='border text-center ps-2 text' style={{"transform": "translateX(8%)"}}>MicroTESE Performed</p>
+        <Col  className=' ' >
+            <h1 className='ps-1 circle mx-auto'>1600+</h1>
+            <p className=' text-center ps-2 text' style={{"transform": "translateX(8%)"}}>MicroTESE Performed</p>
         </Col>
 
     </Row>
